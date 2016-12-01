07608df4ef7ec181396fe302622c696ef425cc97
iserbit <iserbit@gmail.com>
2016-12-01 16:58:26 -0500

    Added Recursion Bonus to Digital Root

--- lib/numerical.rb
+++ lib/numerical.rb
@@ -1,26 +1,34 @@
 ### Numerical Practice Problems
 
 # Write a method `primes(num)` returns an array of the first `num` primes
+
 def primes(count)
 end
 
 # Write a method that returns the factors of a number in ascending order.
+
 def factors(num)
 end
 
 # Write a method, `digital_root(num)`. It should Sum the digits of a positive
 # integer. If it is greater than 10, sum the digits of the resulting number.
-# Keep repeating until there is only one digit in the result, called the
-# "digital root". **Do not use string conversion within your method.**
+# Keep repeating until there is only one digit in the result.
+# **Do not use string conversion within your method.**
+#
+# Bonus - Solve it recursively
+# Bonus 2 - Solve it in a single line
+
 def digital_root(num)
 end
 
 # Recursively calculate the sum of an array of values
+
 def sum_rec(nums)
 end
 
 # Write a **recursive** implementation of a method that returns the
 # first `n` factorial numbers.
 # n_factorials(6) == [0!, 1!, 2!, 3!, 4!, 5!]
+
 def factorials_rec(num)
 end
