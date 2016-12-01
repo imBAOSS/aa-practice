3545524f2a64dd592b0d982cc9d263f4c741c221
iserbit <iserbit@gmail.com>
2016-11-30 23:57:31 -0500

    Added &prc Calls

--- solutions/sort.rb
+++ solutions/sort.rb
@@ -7,12 +7,24 @@
 class Array
 
   def bubble_sort(&prc)
+    dup.bubble_sort!(&prc)
+  end
+
+  def bubble_sort!(&prc)
   end
 
   def merge_sort(&prc)
+    dup.merge_sort!(&prc)
+  end
+
+  def merge_sort!(&prc)
   end
 
   def quick_sort(&prc)
+    dup.quick_sort!(&prc)
+  end
+
+  def quick_sort!(&prc)
   end
 
 end
